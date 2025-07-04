5bdd8d9c351c4476f257671d909e53f3161542b9
Arinji2 <arinjaydhar205@gmail.com>
2025-07-04 11:40:48 +0530

    Add Spotify package

--- api/go.mod
+++ api/go.mod
@@ -10,5 +10,11 @@ require (
 
 require (
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.5.0 // indirect
+	github.com/zmb3/spotify/v2 v2.4.3 // indirect
+	golang.org/x/net v0.23.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 )
--- api/handler/token.go
+++ api/handler/token.go
@@ -2,24 +2,32 @@ package handler
 
 import (
 	"context"
-	"os"
+	"fmt"
 
 	"github.com/arinji2/spotify-ui-api/internal/gen"
+	"github.com/arinji2/spotify-ui-api/internal/openapi"
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"golang.org/x/oauth2/clientcredentials"
 )
 
 func (h *Handler) GetToken(ctx context.Context, request gen.GetTokenRequestObject) (gen.GetTokenResponseObject, error) {
-	expected := os.Getenv("API_SECRET")
-
-	if request.Params.Secret != expected {
-		return gen.GetToken401JSONResponse{
-			Error: "Invalid secret",
-		}, nil
+	config := &clientcredentials.Config{
+		ClientID:     h.Env.Spotify.ClientID,
+		ClientSecret: h.Env.Spotify.ClientSecret,
+		TokenURL:     spotifyauth.TokenURL,
 	}
 
-	// Do Stuff
+	httpClient := config.Client(ctx)
+
+	client := spotify.New(httpClient)
+	token, err := client.Token()
+	if err != nil {
+		return openapi.InternalError(fmt.Errorf("error with getting token: %v", err).Error(), "Error With Authenticating to Spotify"), nil
+	}
+
 	return gen.GetToken200JSONResponse{
-		AccessToken: "mock-token",
-		ExpiresIn:   3600,
-		TokenType:   "Bearer",
+		AccessToken: token.AccessToken,
+		ExpiresIn:   int(token.Expiry.Unix()),
 	}, nil
 }
