10a24d7b4c8a13ebe76a35a10c7af8d4074b2a95
Arinji2 <arinjaydhar205@gmail.com>
2025-07-04 20:10:24 +0530

    Remove internal error thingy

--- api/internal/openapi/response.go
+++ api/internal/openapi/response.go
@@ -1,24 +1,6 @@
 // Package openapi provides primitives to interact with the openapi HTTP API.
 package openapi
 
-import "github.com/arinji2/spotify-ui-api/internal/gen"
-
 func StringPtr(s string) *string {
 	return &s
-}
-
-func InternalError[T any](msg, userMsg string) T {
-	var resp T
-	switch v := any(&resp).(type) {
-	case *gen.GetToken500JSONResponse:
-		*v = gen.GetToken500JSONResponse{
-			InternalErrorJSONResponse: gen.InternalErrorJSONResponse{
-				Error:          msg,
-				DisplayMessage: StringPtr(userMsg),
-			},
-		}
-	default:
-		panic("InternalError: unsupported response type")
-	}
-	return resp
 }
