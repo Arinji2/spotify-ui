063e0565b865e8da892ea82723cdd326dea30008
Arinji2 <arinjaydhar205@gmail.com>
2025-07-05 20:31:58 +0530

    Finish Playlist fetch

---
+++ frontend/src/utils/client.ts
@@ -0,0 +1,8 @@
+import createClient from 'openapi-fetch'
+import type { paths } from '@/../generated/openapi-schema'
+
+export function getAPIClient() {
+  const apiURL = process.env.API_URL
+  const client = createClient<paths>({ baseUrl: apiURL })
+  return client
+}
